ebddcc00da6cfe85f64bdec9ab0c372bc4ff62c2
ronttizz <toni.nissinen@gmail.com>
2022-05-10 13:10:32 +0300

    Added information boxes to homepage and some links to guide the user

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import video from "../assets/video/cooking.mp4";
 import Box from "./Box";
 
@@ -9,7 +10,12 @@ const Home = () => {
         <h1 className="tasteit">TasteIt</h1>
         <p className="description">
           TasteIt recipe app made with React at{" "}
-          <a href="https://www.bc.fi" target="_blank" rel="noreferrer" className="link">
+          <a
+            href="https://www.bc.fi"
+            target="_blank"
+            rel="noreferrer"
+            className="linkactive hover"
+          >
             Business College Helsinki
           </a>
         </p>
@@ -17,8 +23,32 @@ const Home = () => {
       <div className="content">
         <h4>Looking for recipes?</h4>
         <div className="boxcontainer">
-          <Box boxheader="Test box">This is a test box</Box>
-          <Box boxheader="Another box">This is another test box</Box>
+          <Box boxheader="Browse recipes">
+            Are you looking for recipes? Take a look at our list of recipes in "
+            <NavLink to="/recipes" className="boxlink">
+              Recipes
+            </NavLink>
+            " section.
+          </Box>
+          <Box boxheader="Adding recipe">
+            Do you want to make your own recipe to our database? We'd love to see your
+            recipe there so take your time and add your recipe{" "}
+            <NavLink to="/recipes" className="boxlink">
+              here!
+            </NavLink>
+          </Box>
+          <Box boxheader="Want to know more?">
+            Do you want to know more about the program? Head over to Business College
+            Helsinki website to our program from{" "}
+            <a
+              href="https://en.bc.fi/qualifications/full-stack-web-developer-program/"
+              target="_blank"
+              rel="noreferrer"
+              className="boxlink"
+            >
+              here.
+            </a>
+          </Box>
         </div>
       </div>
     </main>
