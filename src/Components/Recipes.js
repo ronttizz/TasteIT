c67e53d33fc90f185460f1f58b9cc6e79fb6ebd1
ronttizz <toni.nissinen@gmail.com>
2022-05-17 13:30:24 +0300

    Added searchbar functionality

--- src/Components/Recipes.js
+++ src/Components/Recipes.js
@@ -5,6 +5,7 @@ import Axios from "axios";
 const Recipes = () => {
   const [recipes, setRecipes] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     Axios.get("http://localhost:3010/recipes")
@@ -15,6 +16,10 @@ const Recipes = () => {
       .catch((err) => console.log("Error with the database ", err));
   }, []);
 
+  const search = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
     <div className="main">
       <div className="searchbar">
@@ -24,13 +29,23 @@ const Recipes = () => {
           type="text"
           name="search"
           placeholder="Search recipes by name"
+          onChange={search}
         ></input>
       </div>
       <div className="recipecontainer">
         {!loading ? (
-          recipes.map((recipe) => {
-            return <RecipeCard {...recipe} key={recipe.id} />;
-          })
+          recipes
+            .filter((recipe) => {
+              if (
+                recipe.name.toLowerCase().includes(searchTerm.toLowerCase().trim()) ||
+                recipe.author.toLowerCase().includes(searchTerm.toLowerCase().trim())
+              ) {
+                return recipe;
+              }
+            })
+            .map((recipe) => {
+              return <RecipeCard {...recipe} key={recipe.id} />;
+            })
         ) : (
           <p>Loading recipes...</p>
         )}
